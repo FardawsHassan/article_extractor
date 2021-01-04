242438495da54d5cc36b6b7c0d3a173e4be6e701
FardawsHassan <fardawshassanshuvro@gmail.com>
2021-01-05 03:36:51 +0600

    getting things ready for upload in heroku

--- app.py
+++ app.py
@@ -1,10 +1,46 @@
-import urllib.request
+from flask import Flask, request, render_template, jsonify
+from flask_restful import Api, Resource
 from newspaper import Article
 
-url="https://newspaper.readthedocs.io/en/latest/"
+app = Flask(__name__)
+api = Api(app)
 
-article = Article(url,keep_article_html=True)
-article.download()
-article.parse()
 
-print(article.text)
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+class main(Resource):
+    def get(self):
+        return render_template('index.html')
+
+
+class send(Resource):
+    def post(self):
+        try:
+            data = request.form
+            url = data['messege']
+            #url="https://newspaper.readthedocs.io/en/latest/"
+            article = Article(url,keep_article_html=True)
+            article.download()
+            article.parse()
+        except:
+            return {               
+                'messege':"Data not found.",
+                'status':404
+            }
+        else:
+            retJson = {
+                "article": article.text,
+                "status" : 200
+            }
+            return jsonify(retJson)
+    def get(self):
+        return 'You are trying to enter from a wrong way. you bad boy....'
+
+
+api.add_resource(send,'/send')
+api.add_resource(main,'/main')
+
+if __name__ == '__main__':
+    app.run(debug=True)
